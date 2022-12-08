76fadf87f91d3169ef70744426efc047df750986
Aymen13h <aymen.kharrat13@gmail.com>
2022-12-08 11:52:11 +0100

    checkpoint react

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <footer>
+            <p>
+                this is footer
+            </p>
+        </footer>
+      </div>
+    )
+  }
+}
